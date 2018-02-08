f26eb9639ca8d3c71c484c02c573e2aa2f96b34a
Alex Dixon <dixonary@martin.wifi-campus.warwick.ac.uk>
2018-02-08 14:23:04 +0000

    minor fixes

--- source/objects/StdDeathTxt.hx
+++ source/objects/StdDeathTxt.hx
@@ -3,21 +3,38 @@ import adventure.*;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
 
+import flixel.FlxG;
 
 
 
 class StdDeathTxt extends Object {
 
-	public function new(x,y){
-		super (x,y);
-		customName = " ";
-		loadGraphic("assets/images/stddeathtxt.png", true,85,32);
-		layer=FORE;
-		visible = false;
+    var diedText:FlxText;
 
-		var text = new FlxText(0,0,500);
-		text.text = "HELLLLLLO";
-		text.setFormat(null, 20, FlxColor.GREEN,CENTER);
-		this.stamp(text);
-	}
+    public function new(x,y){
+        super (x,y);
+        customName = " ";
+        loadGraphic("assets/images/stddeathtxt.png", true,85,32);
+        layer=FORE;
+        visible = false;
+
+        diedText = new FlxText(0,0,500);
+        diedText.text = "YOU DIED";
+        diedText.setFormat(null, 18, FlxColor.WHITE,LEFT);
+        diedText.visible = false;
+        FlxG.state.add(diedText);
+    }
+
+    public function activate() {
+        visible = true;
+        //make diedText visible as well
+        diedText.visible = true;
+
+        // set the position of diedText
+                                // scaled units!
+        diedText.x = this.x + (35 * room.scaleFactor);
+        diedText.y = this.y + (4 * room.scaleFactor);
+
+    }
+
 }
