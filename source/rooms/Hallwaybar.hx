d0bf8a3c5ba1bcb20fc95f6e829506d4f353ae61
Alex Dixon <dixonary@martin.wifi-campus.warwick.ac.uk>
2018-02-08 14:10:41 +0000

    death text

--- source/rooms/Hallwaybar.hx
+++ source/rooms/Hallwaybar.hx
@@ -8,26 +8,25 @@ import flixel.util.FlxColor;
 
 
 class Hallwaybar extends Room {
-	var nudedone = false;
-	var done = false;
+    var nudedone = false;
+    var done = false;
     override public function create() {
-    	var nudedone = false;
+        var nudedone = false;
         //scaleFactor = 8;
         objects = [
-					new RoomTrigger(6,Loohall,90,10),
-					new Block(109),
-					new Nathan(107,11),
-					new Hallwaybardoor(45,8),
-					new Doorman(71,13),
-					new StdDeathTxt(17,6),
-        			new Player(10,10)];
+            new RoomTrigger(6,Loohall,90,10),
+            new Block(109),
+            new Nathan(107,11),
+            new Hallwaybardoor(45,8),
+            new Doorman(71,13),
+            new StdDeathTxt(17,6),
+            new Player(10,10)];
 
     }
 
     override public function update(d){
 
-
-    	if (nudedone){
+        if (nudedone){
             return;
         }
         if(Player.clothed == false){
@@ -37,71 +36,71 @@ class Hallwaybar extends Room {
         }
 
 
-		   if(Player.std == true){
+        if(Player.std == true){
 
-		   	if(done) return;
-		   	done = true;
+            if(done) return;
+            done = true;
 
-		   var player:Player = get(Player);	
-		   var nathan:Nathan = get(Nathan);
+            var player:Player = get(Player);	
+            var nathan:Nathan = get(Nathan);
 
-		    var stdDeath = [
-				{time:0.0,run:function(){
-					player.say("Ow, something itches.", null, 2);
-					player.canControl = false;
-					Global.canInteract = false;
-					trace(Player.std);
-				}},
+            var stdDeath = [
+            {time:0.0,run:function(){
+                player.say("Ow, something itches.", null, 2);
+                player.canControl = false;
+                Global.canInteract = false;
+                trace(Player.std);
+            }},
 
-				{time:2.0,run:function(){
-					player.walkToObject(Nathan, X, LEFT, function(){
-						player.flipX = true;
-						player.say("This really, really hurts.");
-					});
-				}},
-				{time:6.0,run:function(){
-					player.walkTo(player.x - 500, player.y, function(){
-						player.say("Ow.");
-					});
-				}},
+            {time:2.0,run:function(){
+                player.walkToObject(Nathan, X, LEFT, function(){
+                    player.flipX = true;
+                    player.say("This really, really hurts.");
+                });
+            }},
+            {time:6.0,run:function(){
+                player.walkTo(player.x - 500, player.y, function(){
+                    player.say("Ow.");
+                });
+            }},
 
-				{time:10.0,run:function(){
-					if (Player.clothed == false){
-						player.say("This itches bad.", null, 2);
-					}
-					else{
-						player.say("I'm feeling really warm");
-						player.customAnimation = "undress";
-						player.afterAnimation (function(){
-							player.customAnimation = null;
-							Player.clothed = false;
-						});
-					}
-					
-				}},
-				{time:13.0,run:function(){
-						player.say("Oh god, why is that swollen");
-				}},
-				{time:16.0,run:function(){
-						nathan.say("Mmmpph... Mppph!");
-				}},
-				{time:19.0,run:function(){
-						player.say("What do you mean Space Clap?");
-				}},
-				{time:23.0,run:function(){
-						player.customAnimation = "std";
-							player.afterAnimation (function(){
-							player.customAnimation = "stdIdle";
-							player.say("Momma?",FlxColor.LIME, null);
-						});
-				}},
-				{time:27.0,run:function(){
-					var death:StdDeathTxt = get(StdDeathTxt);	
-					death.visible = true;
-				}},
-    		];
+            {time:10.0,run:function(){
+                 if (Player.clothed == false){
+                     player.say("This itches bad.", null, 2);
+                 }
+                 else{
+                     player.say("I'm feeling really warm");
+                     player.customAnimation = "undress";
+                     player.afterAnimation (function(){
+                         player.customAnimation = null;
+                         Player.clothed = false;
+                     });
+                 }
 
-    		Event.run(stdDeath,false);
-    }
-super.update(d);
-}}
+             }},
+            {time:13.0,run:function(){
+                 player.say("Oh god, why is that swollen");
+             }},
+            {time:16.0,run:function(){
+                 nathan.say("Mmmpph... Mppph!");
+             }},
+            {time:19.0,run:function(){
+                 player.say("What do you mean Space Clap?");
+             }},
+            {time:23.0,run:function(){
+                 player.customAnimation = "std";
+                 player.afterAnimation (function(){
+                     player.customAnimation = "stdIdle";
+                     player.say("Momma?",FlxColor.LIME, null);
+                 });
+             }},
+            {time:27.0,run:function(){
+                 var death:StdDeathTxt = get(StdDeathTxt);	
+                 death.activate();
+             }},
+            ];
+
+            Event.run(stdDeath,false);
+        }
+        super.update(d);
+    }}
