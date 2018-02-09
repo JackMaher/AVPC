2422bd6ceb2509b93fe62d176729ba71b3a636ed
JackMaher <jacksmaher@hotmail.co.uk>
2018-02-09 14:42:10 +0000

    chchchch changes

--- source/Main.hx
+++ source/Main.hx
@@ -12,20 +12,11 @@ class Main extends Sprite
 {
     public function new()
 
-<<<<<<< HEAD
-	{
-		super();
-		Global.defaultScaleFactor = 8;
-        Global.startingRoom = Hallway1;
-		addChild(new FlxGame(1920, 1080, Game));
-	}
-=======
     {
         super();
         Global.defaultScaleFactor = 8;
         Global.startingRoom = Hallway1;
         addChild(new FlxGame(1920, 1080, OpeningScene));
     }
->>>>>>> origin/master
 }
 
