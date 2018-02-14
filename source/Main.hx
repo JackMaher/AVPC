f66a366f2a0052938ca7c59890d1c2f26e58c426
JackMaher <jacksmaher@hotmail.co.uk>
2018-02-14 16:27:51 +0000

    f

--- source/Main.hx
+++ source/Main.hx
@@ -15,7 +15,7 @@ class Main extends Sprite
     {
         super();
         Global.defaultScaleFactor = 8;
-        Global.startingRoom = Hallwaybar;
+        Global.startingRoom = Cutscene;
         addChild(new FlxGame(1920, 1080, Game));
     }
 }
